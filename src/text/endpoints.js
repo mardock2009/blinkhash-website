70c1f9b22f1a6acebc73f86c8f3fcd605c5e7a68
Blinkhash <blinkhashpool@gmail.com>
2021-07-19 15:50:46 -0400

    Establish endpoint framework for API documentation

--- src/text/endpoints.js
+++ src/text/endpoints.js
@@ -1,7 +1,55 @@
 import { flatten } from '../common/utils';
 
 // Text for Module Sections
-export const sections = [];
+export const sections = [
+  {
+    'API Requests': '',
+    'Error Codes': '',
+    'Unknown Requests': '',
+    'Response Data': '',
+  },
+  {
+    'Block Endpoints': '',
+    'Confirmed Blocks': '',
+    'Kicked Blocks': '',
+    'Pending Blocks': '',
+    'Combined Blocks': '',
+  },
+  {
+    'Miner Endpoints': '',
+    'Current Miners': '',
+    'Specific Miners': '',
+  },
+  {
+    'Payment Endpoints': '',
+    'Worker Balances': '',
+    'Immature Payouts': '',
+    'Validated Payouts': '',
+    'Amounts Paid': '',
+    'Payment Records': '',
+    'Combined Payments': '',
+  },
+  {
+    'Round Endpoints': '',
+    'Current Rounds': '',
+    'Specific Rounds': '',
+    'Combined Rounds': '',
+  },
+  {
+    'Statistics Endpoints': '',
+    'Specific Pool Statistics': '',
+  },
+  {
+    'Worker Endpoints': '',
+    'Current Workers': '',
+    'Specific Workers': '',
+  },
+  {
+    'Miscellaneous Endpoints': '',
+    'Current Partners': '',
+    'Current Active Pools': '',
+  }
+];
 
 // Generate Headers from Sections
 export const headers = flatten(sections.map((section) => Object.keys(section)));
